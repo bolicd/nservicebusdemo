7d136a7787ab1699d4eb32e53aacf56eed4dba9a
Damir Bolic <bolicd@gmail.com>
2023-02-17 09:10:33 +0100

    Variant comments added

--- WorkerService1/Variants.cs
+++ WorkerService1/Variants.cs
@@ -2,8 +2,11 @@
 {
     public enum Variants
     {
+        //Works, DTC is enforced (works if database is on different server)
         TransactionScope = 0,
+        //Works, no DTC using ambient connection(doesnt work if databases are on different server!)
         TransactionFromConnection = 1,
+        //Doesnt work, hangs when trying to close the connection
         TransactionScopeWithConnection = 2
     }
 }
